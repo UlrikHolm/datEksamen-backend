05e9195b4e79f06e4ee2b0bce4092fd22aeed7a6
Ulrik Holm <ulrikholmuh@gmail.com>
2020-01-12 08:12:41 +0100

    2nd

---
+++ src/main/java/facades/FacadeExample.java
@@ -0,0 +1,63 @@
+package facades;
+
+import entities.RenameMe;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * Rename Class to a relevant name Add add relevant facade methods
+ */
+public class FacadeExample {
+
+    private static FacadeExample instance;
+    private static EntityManagerFactory emf;
+    
+    //Private Constructor to ensure Singleton
+    private FacadeExample() {}
+    
+    
+    /**
+     * 
+     * @param _emf
+     * @return an instance of this facade class.
+     */
+    public static FacadeExample getFacadeExample(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new FacadeExample();
+        }
+        return instance;
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+    
+    //TODO Remove/Change this before use
+    public long getRenameMeCount(){
+        EntityManager em = emf.createEntityManager();
+        try{
+            long renameMeCount = (long)em.createQuery("SELECT COUNT(r) FROM RenameMe r").getSingleResult();
+            return renameMeCount;
+        }finally{  
+            em.close();
+        }
+        
+    }
+    
+        public long getRequestCategoryCount(String category){
+        EntityManager em = emf.createEntityManager();
+        try{
+            long RequestCount = (long)em.createQuery("SELECT COUNT(r) FROM Reguest r JOIN r.categoryList c WHERE c.name = :category")
+                    .setParameter("category", category)
+                    .getSingleResult();
+            return RequestCount;
+        }finally{  
+            em.close();
+        }   
+    }
+
+}
